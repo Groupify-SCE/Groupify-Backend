b4e46b2f1dc0a00ed18583ed4d2851402c921eb8
NadavMozeson <nadavson02@gmail.com>
2025-02-26 13:40:32 +0200

    Setup basic cors for developing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import { StatusCodes } from 'http-status-codes';
 import authRouter from './routes/auth';
 
 const app = express();
 const port = 3001;
+
+const allowedOrigins = ['http://localhost:3000', 'https://groupify-frontend.onrender.com'];
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -17,7 +32,7 @@ app.use('/auth', authRouter);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
-    return console.log(`Running on http://localhost:${port}`);
+    console.log(`Running on http://localhost:${port}`);
   });
 }
 
