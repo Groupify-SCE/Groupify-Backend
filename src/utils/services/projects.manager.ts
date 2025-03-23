16fcda2d39eaae8f60053644a3c847241b5313d4
MaorMalka <maor4211@gmail.com>
2025-03-23 17:23:56 +0200

    create project

---
+++ src/utils/services/projects.manager.ts
@@ -0,0 +1,54 @@
+import { StatusCodes } from 'http-status-codes';
+import { DatabaseManager } from './database.manager';
+import { ObjectId } from 'mongodb';
+
+class ProjectsManager {
+  private static instance: ProjectsManager;
+  private userDatabaseManager = new DatabaseManager('Users');
+  private projectsDatabaseManager = new DatabaseManager('Projects');
+
+  private constructor() {}
+
+  public static getInstance(): ProjectsManager {
+    if (!ProjectsManager.instance) {
+      ProjectsManager.instance = new ProjectsManager();
+    }
+    return ProjectsManager.instance;
+  }
+
+  public async createProject(
+    userId: string
+  ): Promise<{ status: number; response: string }> {
+    try {
+      const user = await this.userDatabaseManager.findOne({
+        _id: new ObjectId(userId),
+      });
+      if (!user) {
+        return { status: StatusCodes.NOT_FOUND, response: 'User not found' };
+      }
+      const projects = await this.projectsDatabaseManager.find({
+        user: new ObjectId(userId),
+      });
+      const result = await this.projectsDatabaseManager.create({
+        user: new ObjectId(userId),
+        name: `New Project ${projects.length + 1}`,
+        participants: 10,
+        registrants: 0,
+        group_size: 2,
+        preferences: 0,
+      });
+      if (result.acknowledged) {
+        return { status: StatusCodes.OK, response: 'Created project' };
+      }
+    } catch (err) {
+      console.error('Failed to create project:', err);
+    }
+    return {
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+      response: 'Failed to created project',
+    };
+  }
+}
+
+const projectsManager = ProjectsManager.getInstance();
+export default projectsManager;
