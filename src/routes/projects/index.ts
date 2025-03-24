f21cb09ec434706f5b50697c83687957fab9d16f
MaorMalka <maor4211@gmail.com>
2025-03-24 13:04:00 +0200

    added route and validate data

--- src/routes/projects/index.ts
+++ src/routes/projects/index.ts
@@ -1,6 +1,8 @@
 import express, { Router, Request, Response } from 'express';
 import { validateAndExtractAuthToken } from '../../utils/middleware/authToken.middleware';
 import projectsManager from '../../utils/services/projects.manager';
+import { validateData } from '../../utils/middleware/validation.middleware';
+import { projectDeleteSchema } from './types';
 
 const router: Router = express.Router();
 
@@ -27,6 +29,19 @@ router.get(
         userId ?? ''
       );
       res.status(status).send({ response });
+    }
+  );
+  
+  router.delete(
+    '/delete/:projectId',
+    validateAndExtractAuthToken(),
+    validateData(projectDeleteSchema, 'params'),
+    async (req: Request, res: Response) => {
+      const userId = req.userId;
+      const projectId = req.params.projectId;
+  
+      
+      
     }
   );
 
